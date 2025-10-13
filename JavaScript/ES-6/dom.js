37ff1228dc44cd18f5fba98ca4b40f2d400efe38
AnveshSrivastava <anveshsrivastava9@gmail.com>
2025-10-13 12:02:52 +0530

    DOM Structure

---
+++ JavaScript/ES-6/dom.js
@@ -0,0 +1,6 @@
+const newDiv = document.createElement('div')
+const newH1 = document.createElement('h1')
+newH1.innerText = "abes"
+newDiv.appendChild(newH1)
+const root = document.getElementById('root')
+root.appendChild(newDiv)
